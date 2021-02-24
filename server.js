cd428bdaf061695dfd3a15b9e3d287957c55d50b
ebenezer <e.kankam95@hotmail.com>
2021-02-24 17:00:35 -0600

    integrated the server to the front-end

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -94,6 +94,21 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+// home page route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// animal route
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+// zookeepers route
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
 });
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
